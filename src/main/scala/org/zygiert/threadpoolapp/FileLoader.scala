4aad3ad7b5f1216383f4b478a9b4ea22ff8fbf68
Michał Zyga <michal.zyga@softwaremill.com>
2025-12-28 05:11:27 +0100

    introduce short delay for shortIO & increase request timeout in Netty config

--- src/main/scala/org/zygiert/threadpoolapp/FileLoader.scala
+++ src/main/scala/org/zygiert/threadpoolapp/FileLoader.scala
@@ -7,11 +7,12 @@ import scala.io.Source
 object FileLoader extends StrictLogging:
 
   private val fileContent = Source.fromResource("data-10000-words.txt").getLines().toSeq
-  private val delay = 500
+  private val longDelay = 500
+  private val shortDelay = 50
 
   def load(longIO: Boolean): Seq[String] =
     val start = System.nanoTime()
-    Thread.sleep(if (longIO) delay else 0)
+    Thread.sleep(if (longIO) longDelay else shortDelay)
     val end = System.nanoTime()
     logger.debug(s"Reading file took: ${end - start}ns")
     fileContent
--- src/main/scala/org/zygiert/threadpoolapp/Main.scala
+++ src/main/scala/org/zygiert/threadpoolapp/Main.scala
@@ -1,16 +1,21 @@
 package org.zygiert.threadpoolapp
 
-import sttp.tapir.server.netty.NettyFutureServer
+import sttp.tapir.server.netty.{NettyConfig, NettyFutureServer}
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration.*
 import scala.concurrent.{Await, ExecutionContext, Future}
 
 @main def run(): Unit =
 
   val program =
     for
-      binding <- NettyFutureServer().host("0.0.0.0").port(8080).addEndpoints(Endpoints.all).start()
+      binding <- NettyFutureServer()
+        .host("0.0.0.0")
+        .port(8080)
+        .config(NettyConfig.default.copy(requestTimeout = Some(120.seconds)))
+        .addEndpoints(Endpoints.all)
+        .start()
       _ <- Future:
         println(s"Go to http://localhost:${binding.port}/docs to open SwaggerUI.")
         Thread.currentThread().join()
